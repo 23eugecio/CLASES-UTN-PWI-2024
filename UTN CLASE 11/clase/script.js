500d08bb25ae0499fb214bd046099eb391b14034
23eugecio <167218847+23eugecio@users.noreply.github.com>
2024-05-23 12:10:14 -0300

    Add files via upload

---
+++ UTN CLASE 11/clase/script.js
@@ -0,0 +1,19 @@
+/* 
+solicitar al usuario una edad y un lenguaje favorito
+Si su edad es mayor a 16 y su lenguaje favortio es 'JS' 
+mostrar por consola 'Bienvenido al desarrollo web frontend'
+mostrar por consola 'No se puede ser perfecto en todo'
+*/
+
+edad= 16
+let edad = prompt('ingresa-tu-edad')
+let lenguaje_fav = prompt('ingresa tu lenguaje favorito')
+if(edad >16 && lenguaje_fav === "JS"){
+    console.log('Bienvenidos al desarollo web frontend')
+}
+else if(edad > 14 && lenguaje_fav == 'PHP'){
+    console.log('Bienvenido al desarrollo web frontend')
+}
+else{
+    console.log('no se puede ser perfecto en todo')
+}
